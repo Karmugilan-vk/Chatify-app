b7e0c0191f004dc533224421e563f836f6ee51dc
Karmugilan R <karmugilanrama@gmail.com>
2026-02-17 06:39:51 +0530

    signup section code refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -34,8 +34,8 @@ export const signup = async (req,res) => {
         });
 
         if(newUser) {
-            await newUser.save();
-            generateToken(newUser._id, res);
+           const savedUser = await newUser.save();
+            generateToken(savedUser._id, res);
             
             res.status(201).json({
             _id: newUser._id,
