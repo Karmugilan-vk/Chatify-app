b6fd7250caed5d8f785d1e1809a8e4d985ab28a7
Karmugilan R <karmugilanrama@gmail.com>
2026-03-16 22:38:21 +0530

    chat page layout setup

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,10 +5,12 @@ import toast from "react-hot-toast";
 export const useAuthStore = create((set) => ({
   authUser: null,
   isCheckingAuth: true,
+  isSigningUp: false,
+  isLoggingIn: false,
 
   checkAuth: async () => {
     try {
-      const res = await axiosInstance.get("auth/check");
+      const res = await axiosInstance.get("/auth/check");
       set({ authUser: res.data });
     } catch (error) {
       console.log("Error in authCheck:", error);
@@ -30,5 +32,31 @@ export const useAuthStore = create((set) => ({
     } finally {
       set({ isSigningUp: false });
     }
-  }
+  },
+  
+  login : async(data) => {
+    set({isLoggingIn:true})
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+
+      toast.success("Logged in successfully")
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+    logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out succesfully");
+    } catch (error) {
+      toast.error("Error logging out");
+      console.log("Logout error:", error);
+    }
+  },
 }));
+
